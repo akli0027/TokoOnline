ba4755444eb4f94dda5428ef3f8b649420173fc8
akli0027 <73159374+akli0027@users.noreply.github.com>
2020-10-20 20:35:18 +0700

    Update proses_register.php

--- proses_register.php
+++ proses_register.php
@@ -6,7 +6,7 @@ $no_telepon = $_POST['no_telepon'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$query = mysqli_query($connect, "INSERT INTO pembeli VALUES (10,'$nama','$no_telepon','$alamat','$email','$password')");
+$query = mysqli_query($connect, "INSERT INTO pembeli VALUES (NULL,'$nama','$no_telepon','$alamat','$email','$password')");
 
 	header('location: index.php');
 
