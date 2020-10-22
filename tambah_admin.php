fe706d8f88bad322a391a80c967f0f88a9075cea
akli0027 <73159374+akli0027@users.noreply.github.com>
2020-10-22 11:34:40 +0700

    Update tambah_admin.php

--- tambah_admin.php
+++ tambah_admin.php
@@ -1,12 +1,11 @@
 <?php
  include 'koneksi.php';
-$id_admin = $_POST['id_admin'];
 $nama_admin = $_POST['nama_admin'];
 $password_admin = $_POST['password_admin'];
 $email_admin = $_POST['email_admin'];
 
 
-$query = mysqli_query($connect,"INSERT INTO admin VALUES ('$id_admin','$nama_admin','$password_admin','$email_admin')");
+$query = mysqli_query($connect,"INSERT INTO admin VALUES (NULL,'$nama_admin','$password_admin','$email_admin')");
 if ($query) {
   header('location:admin.php');
 }
