cdd2fc1314a0e5d0635baea5a8a48e25731014e7
akli0027 <73159374+akli0027@users.noreply.github.com>
2020-10-22 08:42:26 +0700

    Update home.php

--- home.php
+++ home.php
@@ -71,22 +71,6 @@ if (!isset($_SESSION['id_pembeli'])){
     </div>
   </nav>
 
-  <!-- Masthead -->
-  <header class="masthead">
-    <div class="container h-100">
-      <div class="row h-100 align-items-center justify-content-center text-center">
-        <div class="col-lg-10 align-self-end">
-          <h1 class="text-uppercase text-white font-weight-bold">
-YOUR FAVORITE ITEMS CAN LOOK HERE</h1>
-          <hr class="divider my-4">
-        </div>
-        <div class="col-lg-8 align-self-baseline">
-          <p class="text-white-75 font-weight-light mb-5">Let's see and find yours</p>
-        </div>
-      </div>
-    </div>
-  </header>
-
   <!-- Home Section -->
   <section class="page-section bg-primary" id="home">
     <div class="container">
