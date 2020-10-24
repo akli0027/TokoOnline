2fbc33d2fa45c383f13a38e77be4a7ec7a3be3b0
akli0027 <73159374+akli0027@users.noreply.github.com>
2020-10-24 15:54:46 +0700

    Update kategori_Monitor.php

--- kategori_Monitor.php
+++ kategori_Monitor.php
@@ -133,11 +133,7 @@ BEFORE !! MOVE CATEGORY ADD CART FIRST</h2>
   </tbody>
 </table>
   </form>
-  <center>
-   <a href="invoice.php">
-    <button class="btn btn-primary">Bayar</button>
-   </a>
-</center>
+
 </section>
 
  
