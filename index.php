f966e9db5e0dd1037d679fbd0ad5dafe812cfa8c
akli0027 <73159374+akli0027@users.noreply.github.com>
2020-10-25 17:01:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             <div class="container">
                 <div class="signup-content">
                     <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
+                        <h2 class="form-title">Sign In</h2>
                         <form method="POST" class="register-form" id="register-form" action="proses_register.php">
                              <div class="form-group">
                                 <input type="text" name="nama" placeholder="Your Name"/>
