12afba333a88eddc0df3ad6d4ac721d8f78c1d47
akli0027 <73159374+akli0027@users.noreply.github.com>
2021-06-30 13:19:34 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-			$connect = new mysqli ('db4free.net','laptop_bekas','.GLnvd3yZa89k.u','laptop_bekas');
+			$connect = new mysqli ('db4free.net','laptop_bekas','b5a7c9f2','laptop_bekas');
 			//$connect = new mysqli ('localhost','root','','laptop_bekas');
 if ($connect->connect_error) {
 	//jika terjadi error, matikan proses dengan die() atau exit()
